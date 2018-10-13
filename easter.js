68efb21cc9336ded5ebf04ee490504fe62c17dbf
Wilkin <wilkin.delossantos@csedge.org>
2018-10-13 13:30:38 -0400

    Completed Easter Challenge

--- easter.js
+++ easter.js
@@ -1,5 +1,48 @@
 // import readline-sync module, don't touch!
 const READLINE = require("readline-sync");
-
+let y = READLINE.question("Type the year for Easter: ");
+console.log(`y=${y}`);
 // Write your program below!
 // First line should get user input (year) and store it in variable y.
+ 
+let quotient = Math.floor(y / 19);
+
+
+let a = Math.floor(y % 19);
+console.log(`a=${a}`);
+
+let b =Math.floor (y / 100);
+console.log(`b=${b}`);
+let c =Math.floor (y % 100);
+console.log(`c=${c}`);
+
+let d =Math.floor(b / 4);
+console.log(`d=${d}`);
+let e = Math.floor (b % 4);
+console.log(`e=${e}`);
+
+let f = Math.floor((b + 8)/25);
+console.log(`f=${f}`);
+let g =Math.floor ((b - f + 1)/3);
+console.log(`g=${g}`);
+let h = Math.floor((19*a+b-d-g+15) % 30);
+console.log(`h=${h}`);
+
+let i =Math.floor (c/4);
+console.log(`i=${i}`);
+let k = Math.floor(c % 4);
+console.log(`k=${k}`);
+
+let r = Math.floor((32+2*e+2*i-h-k) % 7);
+console.log(`r=${r}`);
+let m = Math.floor((a+11*h+22*r) / 451);
+console.log(`m=${m}`);
+
+let n = Math.floor((h+r-7*m+114) / 31);
+console.log(`n=${n}`);
+let p = Math.floor((h+r-7*m+114) % 31);
+console.log(`p=${p}`)
+console.log(`The date of Easter in ${y}, was ${n}/${p+1}`);
+
+
+
--- math.js
+++ math.js
@@ -11,26 +11,36 @@ console.log(sum);
 // **** Problem 2 ****
 // Store the difference between sum (above) and 7 in a variable.
 // Print the difference.
+let x = 7;
+let difference = sum - x;
+console.log(difference);
+
 
 
 // **** Problem 3 ****
 // Store the product of difference and 3 in a variable.
 // Print the product.
-
-
+let y = 3; 
+let product = difference * 3;
+console.log(product);
 // **** Problem 4 ****
 // Store the power of the product raised to an exponent of 2 in a variable.
 // Print the power.
+let z = 2;
+let power = product ** z;
+console.log(power);
 
 
 // **** Problem 5 ****
 // Store the quotient of the power divided by 4 in a variable.
 // Print the quotient.
-
-
-// **** Problem 6 ****
+let a = 4;
+let quotient = power / a;                               
+console.log(quotient)// **** Problem 6 ****
 // Store the remainder of the quotient divided by 2 in a variable.
 // Print the remainder.
+let remainder = quotient % z;
+console.log(remainder);
 
 
 // **** Problem 7 ****
