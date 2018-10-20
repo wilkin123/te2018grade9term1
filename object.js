b3fdfe0800601344c4e5c7f5fc080810df49d1a7
Wilkin <wilkin.delossantos@csedge.org>
2018-10-20 12:35:06 -0400

    Finished objects.

---
+++ object.js
@@ -0,0 +1,15 @@
+let villain = {
+	firstName: "Victor",
+	lastName: "Mina",
+	catchPhrase:"oh, cow",
+	greatestCrime:"choking in fortnite"
+};
+let hacker = {
+	firstName: "Wikin",
+	nickName: "Vivid",
+	catchphrase: "got em",
+	archenemy: "Victor Mina"
+};
+console.log(`I'm ${villain.firstName} , ${villain.lastName}`);
+
+console.log(`I'm ${hacker.firstName} , also known as ${hacker.nickName}`);
