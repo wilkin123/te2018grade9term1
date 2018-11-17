5e51bbbc93c9ed83aa18aef91251ee9a0a9d24ff
Wilkin De Los Sanros <wilkin.delossantos@csedge.org>
2018-11-17 15:52:51 -0500

    I fixed the bugs

--- debugging.js
+++ debugging.js
@@ -4,33 +4,36 @@
 
 let x = 3;
 if(x => 3) {
-  X++;
-} Else {
-  x = 2;
-
+  x++;
+} else {
+  x =+ 2;
+}
 console.log(x);
 
 //Program #2: "Bad logic and math"
 
 let money = 28;
-if(money > 50 && money >= 20) {
+if(money >= 50 && money >= 20) {
   money /= 2;
 } else {
-  money /= 0;
+  money /= 1;
 }
 
 
-Program #3: "And what else?"
+//Program #3: "And what else?"
+const READLINE = require("readline-sync");
 
 let num1 = READLINE.question("Enter num1: ");
 let num2 = READLINE.question("Enter num2: ");
 let num3 = READLINE.question("Enter num3: ");
-if(num1 > num2) {
-  console.log(`${num1} is the largest value.`);
+let highnum = 0
+if(num1 >= num2 && num1 >= num3){
+	highnum = num1;
+}   
+if(num2 >= num3 && num2 >= num1) {
+	highnum = num2;  
 }
-if(num2 > num3) {
-  console.log(`${num2} is the largest value.`);
+if(num3 >= num2 && num3 >= num1){
+	highnum = num3
 }
-else {
-  console.log(`${num3} is the largest value.`);
-}
+console.log(`The highest number was ${highnum}`);
